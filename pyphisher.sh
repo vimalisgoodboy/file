8546cef3081bac6b8d4fbdf6d638d5a8a623feb7
vimal <132744852+vimalisgoodboy@users.noreply.github.com>
2025-01-22 17:24:20 +0530

    Update pyphisher.sh

--- pyphisher.sh
+++ pyphisher.sh
@@ -1,5 +1,5 @@
 sudo apt install git python3 php openssh-client -y
-git clone https://github.com/KasRoudra/PyPhisher
+git clone https://github.com/vimalisgoodboy/PyPhisher.git
 cd PyPhisher
 pip3 install -r files/requirements.txt
 python3 pyphisher.py
