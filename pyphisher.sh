11caee3ef25e36625ed109eac2a2884d51b54c5b
vimal <132744852+vimalisgoodboy@users.noreply.github.com>
2025-01-22 20:05:26 +0530

    Delete pyphisher.sh

--- pyphisher.sh
+++
@@ -1,5 +0,0 @@
-sudo apt install git python3 php openssh-client -y
-git clone https://github.com/vimalisgoodboy/PyPhisher.git
-cd PyPhisher
-pip3 install -r files/requirements.txt
-python3 pyphisher.py
