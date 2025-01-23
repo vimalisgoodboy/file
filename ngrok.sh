fa3d22daca790669af2522914652be0588fc5354
vimal <132744852+vimalisgoodboy@users.noreply.github.com>
2025-01-23 08:54:01 +0530

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -5,3 +5,8 @@ curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
 	&& sudo apt update \
 	&& sudo apt install ngrok
 ngrok config add-authtoken 2s0omvPiWnlmiIdYIZ2tQ7D5z8l_zGuQt3SL5tmrLSrQ6f36
+echo "Enter the port  to run :eg ngrok http 2525"
+read port
+
+# Step 2: Create the directory using the user input
+ngrok http $port
